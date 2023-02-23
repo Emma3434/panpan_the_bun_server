f13276e9f74d53f80ba8badb0945f817f66ba75e
Emma Wang <lemonmilklalala@163.com>
2023-02-22 23:32:35 -0500

    image post and put try

--- app.js
+++ app.js
@@ -1,54 +1,39 @@
-// Load express module
 const express = require('express');
-
-const bodyParser = require('body-parser');
-
-// to ensure client side run on same machine
-const cors = require("cors");
-
-// Initialize app
+const multer = require('multer');
+const cors = require('cors');
 const app = express();
-
-// Parse incoming requests with JSON payloads
-app.use(bodyParser.json());
-
-// Mongoose connection
-require('dotenv').config()
 const mongoose = require('mongoose');
+
+require('dotenv').config();
 mongoose.connect(process.env.MONGODB_URI);
+
 const db = mongoose.connection;
 
-// Check for DB connection
-db.once('open', function(){
-    console.log("Connected to MongoDB successfully!");
-});
-db.on('error', function(){
-    console.log(err);
+db.once('open', function() {
+  console.log('Connected to MongoDB successfully!');
 });
 
-// to ensure client side run on same machine
+db.on('error', function() {
+  console.log(err);
+});
+
+app.use(express.json());
 app.use(cors());
 
-// Route for home
-app.get('/', function (req, res) {
-    res.send('hello world')
+const storage = multer.memoryStorage();
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 1000000 }, // set file size limit to 1MB
 });
 
-// Start server with port 3000
-const port = process.env.PORT || 3000;
-app.listen(port, function(){
-    console.log("Server started on localhost:3000");
+app.get('/', function(req, res) {
+  res.send('hello world');
 });
 
-const diaries = require('./routes/diaries');
-app.use('/diaries', diaries);
-
-const about = require('./routes/about');
-app.use('/about', about);
-
-const epanpan = require('./routes/epanpan');
-app.use('/epanpan', epanpan);
-
-//images
 const image = require('./routes/image');
 app.use('/image', image);
+
+const port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log('Server started on localhost:3000');
+});
--- routes/image.js
+++ routes/image.js
@@ -1,9 +1,10 @@
 const express = require('express');
-
 const Image = require('../models/image.js');
+const multer = require('multer');
 
-// Initialize app
 const router = express.Router();
+const upload = multer({ dest: 'uploads/' });
+
 router.get('/', function (req, res) {
     let image = Image.find({}, function(err, image){
         if(err){
@@ -15,25 +16,57 @@ router.get('/', function (req, res) {
         }
     });
 });
+
 router.get('/:id', function (req, res) {
     const id = req.params.id;
 
     console.log(id);
-  
+
     Image.findById(id, (err, image) => {
-      if (err) {
-        console.log(err);
-        res.sendStatus(500);
-      } else {
-        // if (image) {
+        if (err) {
+            console.log(err);
+            res.sendStatus(500);
+        // } else if (image && image.data) {
         //   res.contentType(image.contentType);
         //   res.send(image.data);
-        // } else {
-        //   res.sendStatus(404);
-        // }
-        res.json(image)
-      }
+        } else {
+          res.json(image);
+          // res.sendStatus(404);
+        }
     });
+});
+
+router.post('/', upload.single('img'), function(req, res) {
+  const newImage = new Image({
+    img_id: req.body.img_id,
+    img: {
+      data: req.file.buffer,
+      contentType: req.file.mimetype
+    }
   });
+  newImage.save(function(err, image) {
+    if (err) {
+      console.log(err);
+      res.sendStatus(500);
+    } else {
+      res.json(image);
+    }
+  });
+});
+
+
+router.put('/:id', async function(req, res) {
+    try {
+        const updatedImage = await Image.findByIdAndUpdate(req.params.id, {
+            img: req.body.img
+        }, { new: true });
+        if (!updatedImage) {
+            return res.status(404).json({ message: 'Image not found' });
+        }
+        res.status(200).json(updatedImage);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+});
 
 module.exports = router;
